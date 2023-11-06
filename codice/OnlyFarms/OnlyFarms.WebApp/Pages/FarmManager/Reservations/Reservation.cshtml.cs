f10ba17cf0b983605399e039b39e27d2fce9bc4e
Stefano De Ciechi <20034883@studenti.uniupo.it>
2023-11-06 19:06:25 +0100

    convertita la pagina ReservationId creata da Robert in un modal+form in Reservation

--- codice/OnlyFarms/OnlyFarms.WebApp/Pages/FarmManager/Reservations/Reservation.cshtml.cs
+++ codice/OnlyFarms/OnlyFarms.WebApp/Pages/FarmManager/Reservations/Reservation.cshtml.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.JSInterop;
 
 namespace OnlyFarms.WebApp.Pages.FarmManager.Reservations
 {
 	public class ReservationModel : PageModel
     {
         private readonly ICompanyRepository<WaterCompany> _companies;
-        private IReservationRepository _reservationRepository;
+        private readonly IReservationRepository _reservations;
+        
+        [BindProperty]
+        public Reservation Reservation { get; set; }
         
         public IEnumerable<WaterCompany> Companies { get; set; }
 
-        public ReservationModel(ICompanyRepository<WaterCompany> companies, IReservationRepository reservationRepository)
+        public ReservationModel(ICompanyRepository<WaterCompany> companies, IReservationRepository reservations)
         {
             _companies = companies;
-            _reservationRepository = reservationRepository;
+            _reservations = reservations;
             
             Companies = Enumerable.Empty<WaterCompany>();
         }
@@ -22,6 +26,23 @@ namespace OnlyFarms.WebApp.Pages.FarmManager.Reservations
         {
             // TODO aggiungere un filtro per citta'?
             Companies = _companies.GetAll().ToBlockingEnumerable();
+        }
+        
+        public async Task<IActionResult> OnPostReservation()
+        {
+            var farmingCompanyId = Reservation.FarmingCompanyId;
+            var waterCompanyId = Reservation.WaterCompanyId;
+            
+            var result = _companies.Get(waterCompanyId).Result;
+            
+            if (!ModelState.IsValid || Reservation.BookedQuantity > result.WaterSupply)     // TODO qui bisognerebbe anche testare se esiste un WaterLimit e confrontarlo con la quantita' richiesta
+            {
+                // se si inseriscono valori non validi per la quantita' si viene reindirizzati alla stessa pagina ma impostando nella query dei valori di errore che verranno mostrati con una sorta di Alert di Bootstrap
+                return RedirectToPage("/FarmManager/Reservations/Reservation", new { farmingCompanyId, errors = true, errorMessage = "The quantity of water inserted is not available for this water company" });
+            }
+
+            await _reservations.Add(farmingCompanyId, waterCompanyId, Reservation);
+            return RedirectToPage("/FarmManager/Reservations/Index", new { farmingCompanyId });
         }
         
     }
