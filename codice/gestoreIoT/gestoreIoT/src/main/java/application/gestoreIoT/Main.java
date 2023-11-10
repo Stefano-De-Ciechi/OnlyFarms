c51c8e2b5d27dcdccf3c1dbe07dc055121ae08f7
robertmelente <20028626@studenti.uniupo.it>
2023-11-10 18:57:04 +0100

    correzione comunicazione mqtt

--- codice/gestoreIoT/gestoreIoT/src/main/java/application/gestoreIoT/Main.java
+++ codice/gestoreIoT/gestoreIoT/src/main/java/application/gestoreIoT/Main.java
@@ -1,21 +1,51 @@
 package application.gestoreIoT;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
+import java.util.Scanner;
+
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class Main {
 
-	public static void main(String[] args) throws MqttException {
-		 GestoreIoT gIT = new GestoreIoT();
-		 String dataTopic = "sensors/data";
-		 String commandTopic = "actuators/command";
-		 String cropId = "1/";
-		
-		 gIT.sendCommand("1", dataTopic);
-		 
-		while(true) {
-			 gIT.receiveMessage(cropId.concat(dataTopic), cropId.concat(commandTopic));
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
 
-		 }
+		 GestoreIoT gIT = new GestoreIoT();
+		 String dataTopic = "command/sensors";
+		 String dataTopicSend = "sensors/data/send/#";
+		 
+		 
+		 String messagePayload = "payload";
+
+		 MqttMessage mqttMessage = new MqttMessage();
+		 mqttMessage.setPayload(messagePayload.getBytes());
+		 
+		 
+		 String commandTopic = "actuators/command";
+		 String scelta = "";
+		 
+		do {
+			 System.out.println("Inserisci il periodo del giorno di cui preferisci ricevere le misurazioni");
+			 System.out.println("Mattina, Pomeriggio, Sera");
+			 System.out.println("Inserisci 0 per concludere la visualizzazione");
+			 scelta = scanner.nextLine();
+			 
+			 
+			gIT.sendCommand(scelta, dataTopic);
+			//gIT.messageArrived(dataTopicSend, mqttMessage);
+			gIT.receiveMessage(dataTopicSend, commandTopic);
+			
+			Thread.sleep(1000);
+			
+			System.out.println("Dati da mandare all'act" +gIT.getData());
+			gIT.sendCommand(gIT.getData(), commandTopic);
+			
+		}while(!scelta.equals("0"));
+		
+		gIT.disconnect();
+		System.exit(0);
+		
+
+		scanner.close();
 	}
 
 }
